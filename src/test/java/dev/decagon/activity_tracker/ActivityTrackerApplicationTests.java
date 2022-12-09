167d4e513fd53e09e2ca3979e6d875a0836e5686
Yome <yomeekoko1@gmail.com>
2022-12-09 12:23:26 +0100

    change

--- src/test/java/dev/decagon/activity_tracker/ActivityTrackerApplicationTests.java
+++ src/test/java/dev/decagon/activity_tracker/ActivityTrackerApplicationTests.java
@@ -1,13 +1,13 @@
-package dev.decagon.activity_tracker;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ActivityTrackerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package dev.decagon.activity_tracker;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ActivityTrackerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
