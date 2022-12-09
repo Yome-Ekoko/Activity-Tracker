1746bf261574c9835c1b5197ccd6a700dd130267
Yome <yomeekoko1@gmail.com>
2022-12-09 10:18:59 +0100

    latest commit

--- src/test/java/dev/decagon/activity_tracker/services/serviceimpls/TaskServiceImplTest.java
+++ src/test/java/dev/decagon/activity_tracker/services/serviceimpls/TaskServiceImplTest.java
@@ -106,3 +106,10 @@ class TaskServiceImplTest {
     }
 
 }
+//echo "# activity-tracker" >> README.md
+//        git init
+//        git add README.md
+//        git commit -m "first commit"
+//        git branch -M main
+//        git remote add origin git@github.com:Yome-Ekoko/activity-tracker.git
+//        git push -u origin main
